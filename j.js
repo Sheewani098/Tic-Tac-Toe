933a41b9dc3cd3a6ae111c96fc1648af82850f87
sushmi <susmitareddy149@gmail.com>
2024-05-19 01:26:04 +0530

    first commit

---
+++ j.js
@@ -0,0 +1,16 @@
+<body>
+<script>
+x=100;
+document.write(x+"<br/>");
+console.log(x);
+var x1= 2000;
+docuent.write(x1+"<br/>");
+console.log(x1);
+let sub=science
+document.write(sub+"<br/>");
+console.log(sub);
+const x6=true;
+document.write(x6+"<br/>")
+console(x6);
+</script>
+</body>
---
+++ num.js
@@ -0,0 +1,47 @@
+/*let num = prompt("enter the number:");
+
+if(num % 23 === 0){
+    console.log(num,"is multiple of 23");
+}else{
+    console.log(num,"is not mutiple of 23");
+}*/
+/*
+
+let score = prompt("enter the number");
+
+if(score>=80 && score<=100){
+    console.log("A grade");
+}else if(score>=70 && score<=89){
+    console.log("b grade");
+}else if(score>=60 && score<=69){
+    console.log("c grade");
+}else if(score>=50 && score<=59){
+    console.log("d grade")
+}else{
+    console.log("f grade");
+}
+*/
+//For of loop
+/*
+let str="javaascript";
+let size=0;
+for(let i of str){
+    console.log("i=",i);
+    size++;
+}
+
+console.log("sstring size =",size);
+*/
+/*
+let str="javaascript";
+for(let i of str){
+    console.log("i=",i);
+}
+*/
+let num = prompt("enter the no");
+if (num % 2 == 0) {
+    console.log("even no");
+    else{
+        console.log("odd no");
+    }
+}
